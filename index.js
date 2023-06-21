2ab803e2019acb895d05c8252aef1d91a2273b56
Jonathan Krall Olsson <jonathan.krall@lexicon.se>
2023-06-21 15:18:48 +0200

    tried to help amir

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {greet} from 'krall-npmtest';
 import {greetldk} from 'npm_test_lidkoping';
+import {calculateProduct} from '@aso200/calculator-result';
 
 window.greet = greet;
-window.greetldk = greetldk; 
+window.greetldk = greetldk;
+window.calculate = calculateProduct;
